1fb7f723feb0d13c377be58127d14d7f4d58d929
G I I K <giikwebdeveloper@gmail.com>
2025-11-14 19:49:39 +0100

    Initial Commit

---
+++ MediumMetal4View/MediumMetal4ViewApp.swift
@@ -0,0 +1,17 @@
+//
+// MediumMetal4ViewApp.swift
+//
+// Created for MediumMetal4View
+//     on 11/14/25
+//     by GIIK Web Development
+
+import SwiftUI
+
+@main
+struct MediumMetal4ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
